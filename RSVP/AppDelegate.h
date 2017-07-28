dc24e9f5cfc8b8d6bf9df5e211024c56b07f3bed
Arshdeep Singh <arshdeepsingh@arshdeeps-macbook-pro.local>
2017-07-28 23:53:29 +0530

    Initial Commit

---
+++ RSVP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSVP
+//
+//  Created by Arshdeep Singh on 28/07/17.
+//  Copyright © 2017 Arshdeep Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RSVP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RSVP
+//
+//  Created by Arshdeep Singh on 28/07/17.
+//  Copyright © 2017 Arshdeep Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
